8f845738ac54efe230bd65819bf2958121dc2cc3
Cassian Godsted <cassian@godsted.com>
2025-07-25 16:37:32 -0400

    animated JEI arrow + cook time

--- src/main/java/com/ncpbails/modestmining/integration/ForgingShapedRecipeCategory.java
+++ src/main/java/com/ncpbails/modestmining/integration/ForgingShapedRecipeCategory.java
@@ -1,16 +1,25 @@
 package com.ncpbails.modestmining.integration;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.ncpbails.modestmining.ModestMining;
 import com.ncpbails.modestmining.block.ModBlocks;
+import com.ncpbails.modestmining.recipe.ForgeRecipe;
 import com.ncpbails.modestmining.recipe.ForgeShapedRecipe;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -22,10 +31,48 @@ public class ForgingShapedRecipeCategory implements IRecipeCategory<ForgeShapedR
 
     private final IDrawable background;
     private final IDrawable icon;
+    private final int regularCookTime = 400;
+    private final LoadingCache<Integer, IDrawableAnimated> cachedArrows;
 
     public ForgingShapedRecipeCategory(IGuiHelper helper) {
         this.background = helper.createDrawable(TEXTURE, 0, 0, 120, 60);
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.FORGE.get()));
+        this.cachedArrows = CacheBuilder.newBuilder()
+                .maximumSize(25)
+                .build(new CacheLoader<>() {
+                    @Override
+                    public IDrawableAnimated load(Integer cookTime) {
+                        return helper.drawableBuilder(TEXTURE, 123, 0, 23, 18)
+                                .buildAnimated(cookTime, IDrawableAnimated.StartDirection.LEFT, false);
+                    }
+                });
+    }
+
+    @Override
+    public void draw(ForgeShapedRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack poseStack, double mouseX, double mouseY) {
+        IDrawableAnimated arrow = getArrow(recipe);
+        arrow.draw(poseStack, 63, 4);
+        drawCookTime(recipe, poseStack, 50);
+    }
+
+    protected void drawCookTime(ForgeShapedRecipe recipe, PoseStack poseStack, int y) {
+        int cookTime = recipe.getCookTime();
+        if (cookTime > 0) {
+            int cookTimeSeconds = cookTime / 20;
+            Component timeString = Component.translatable("gui.jei.category.smelting.time.seconds", cookTimeSeconds);
+            Minecraft minecraft = Minecraft.getInstance();
+            Font fontRenderer = minecraft.font;
+            int stringWidth = fontRenderer.width(timeString);
+            fontRenderer.draw(poseStack, timeString, getWidth() - stringWidth, y, 0xFF808080);
+        }
+    }
+
+    protected IDrawableAnimated getArrow(ForgeShapedRecipe recipe) {
+        int cookTime = recipe.getCookTime();
+        if (cookTime <= 0) {
+            cookTime = regularCookTime;
+        }
+        return this.cachedArrows.getUnchecked(cookTime);
     }
 
     @Override
